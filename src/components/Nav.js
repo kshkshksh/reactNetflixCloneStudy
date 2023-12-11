fa41e54b29ee67a5a0f19357321eac0c6cf8f6c9
unknown <ksh931101@naver.com>
2023-12-11 15:56:15 +0900

    1211_1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,6 @@ export default function Nav() {
 
     useEffect(()=>{
         window.addEventListener("scroll",()=>{
-                console.log(window.scrollY);
             if(window.scrollY>50){
                 setShow(true);
             } else{
@@ -21,17 +20,17 @@ export default function Nav() {
 
 
   return (
-    <nav className={`nav ${show && "nav_black"}`}>
-      <img 
-        alt='Netflix logo'
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png'
-        className='nav_logo'
-        onClick={()=>window.location.reload()}
+    <nav className={`nav ${show && "nav__black"} `}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => (window.location.href = "/")}
       />
       <img
-        alt='User logged'
-        src='https://item.kakaocdn.net/do/fc93afd0e2ae15fb1f03a87e6b0458dcf43ad912ad8dd55b04db6a64cddaf76d'
-        className='nav_avatar'
+        alt="User logged"
+        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+        className="nav__avatar"
       />
     </nav>
   )
