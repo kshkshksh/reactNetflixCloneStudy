41f5060feb6a501a4269077ce2e3506fd4299e61
unknown <ksh931101@naver.com>
2023-12-15 14:46:04 +0900

    1215_1

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,24 +1,25 @@
-import axios from '../api/axios';
-import React, { useEffect, useState } from 'react'
+import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
 import requests from "../api/requests";
 import "./Banner.css";
-import styled from 'styled-components';
-
+import styled from "styled-components";
 export default function Banner() {
   const [movie, setMovie] = useState([]);
   const [isClicked, setIsClicked] = useState(false);
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    //현재 상영중인 영화 정보를 가져오기(여러 영화)
+    // 현재 상영중인 영화 정보를 가져오기(여러 영화)
     const request = await axios.get(requests.fetchNowPlaying);
 
     // 여러 영화 중 영화 하나의 ID를 가져오기
-    const movieId = request.data.results[
-      Math.floor(Math.random() * request.data.results.length)
-    ].id;
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
 
     // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
@@ -29,34 +30,39 @@ export default function Banner() {
 
   const truncate = (str, n) => {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-  }
+  };
 
+  console.log('movie',movie);
   if (!isClicked) {
     return (
       <header
-        className='banner'
+        className="banner"
         style={{
           backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
           backgroundPosition: "top center",
-          backGroundSize: "cover"
+          backgroundSize: "cover",
         }}
       >
         <div className="banner__contents">
-          <h1 className='banner__title'>{movie.title || movie.name || movie.original_name}</h1>
-
-          <div className='banner__buttons'>
-            <button className='banner__button play'
-              onClick={() => setIsClicked(true)}>Play</button>
-            <button className='banner__button info'>More Information</button>
-          </div>
-
-          <h1 className='banner__description'>
-            {truncate(movie?.overview, 100)}
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
           </h1>
 
-        </div>
-        <div className='banner--fadeBottom' />
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Information</button>
+          </div>
 
+          <h1 className="banner__description">
+            {truncate(movie.overview, 100)}
+          </h1>
+        </div>
+        <div className="banner--fadeBottom" />
       </header>
     );
   } else {
@@ -76,37 +82,35 @@ export default function Banner() {
       </Container>
     );
   }
-
 }
 
 const Iframe = styled.iframe`
   width: 100%;
-  height : 100%
-  z-index : -1;
-  opacity : 0.65;
-  border : none;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
 
   &::after {
-    content:"";
-    position : absolute;
-    top:0;
-    left:0;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
   }
-`
+`;
 
 const Container = styled.div`
-  display :flex;
-  justify-content : center;
-  align-items : center;
-  flex-direction : column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   width: 100%;
-  height : 100vh;
-`
+  height: 100vh;
+`;
 
 const HomeContainer = styled.div`
-  width:100%;
-  height : 100vh
-`
-
+  width: 100%;
+  height: 100%;
+`;
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,29 +1,33 @@
 import axios from '../../api/axios';
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import "./SearchPage.css";
+import { useDebounce } from '../../hooks/useDebounce';
 
 export default function SearchPage() {
+
+    const navigate = useNavigate();
 
     const [searchResults, setSearchResults] = useState([]);
 
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     }
-
     let query = useQuery();
     const searchTerm = query.get("q")
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
     useEffect(() => {
-        if (searchTerm) {
-            fetchSearchMovie(searchTerm);
+        if (debouncedSearchTerm) {
+            fetchSearchMovie(debouncedSearchTerm);
         }
-    }, [searchTerm]);
+    }, [debouncedSearchTerm]);
 
     const fetchSearchMovie = async (searchTerm) => {
+        console.log('searchTerm', searchTerm);
         try {
             const request = await axios.get(
-                `/search/multi?include_adult=false&query=${searchTerm}`)
+                `/search/multi?include_adult=false&query=${searchTerm}`);
             console.log(request);
             setSearchResults(request.data.results);
         } catch (error) {
@@ -32,20 +36,22 @@ export default function SearchPage() {
     }
 
     const renderSearchResults = () => {
-        return searchResults.lenth > 0 ? (
+        return searchResults.length > 0 ? (
             <section className='search-container'>
                 {searchResults.map((movie) => {
                     if (movie.backdrop_path !== null && movie.media_type !== "person") {
                         const movieImageUrl =
                             "http://image.tmdb.org/t/p/w500" + movie.backdrop_path
                         return (
-                            <div className='movie'>
+                            <div className='movie' key={movie.id}>
                                 <div
+                                    onClick={() => navigate(`/${movie.id}`) }
                                     className='movie__column-poster'
                                 >
                                     <img
-                                    src={movieImageUrl} alt="movie"
-                                    className='movie__poster'
+                                        src={movieImageUrl}
+                                        alt="movie"
+                                        className='movie__poster'
                                     />
 
                                 </div>
@@ -58,7 +64,7 @@ export default function SearchPage() {
             <section className='no-results'>
                 <div className='no-results__text'>
                     <p>
-                        찾고자하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.
+                        찾고자하는 검색어 "{debouncedSearchTerm}"에 맞는 영화가 없습니다.
                     </p>
                 </div>
             </section>
