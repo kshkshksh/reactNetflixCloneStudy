1e2b2fdcd67872ebf4bf457c50ce5680bda62e8b
unknown <ksh931101@naver.com>
2023-12-18 09:50:55 +0900

    1218

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import React from 'react';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import { Outlet, Route ,Routes} from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import DetailPage from './pages/DetailPage';
-import SearchPage from './pages/SearchPage';
+import { Outlet, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
 
-const Layout = () =>{
-  return(
+const Layout = () => {
+  return (
     <div>
       <Nav />
 
@@ -16,20 +15,19 @@ const Layout = () =>{
 
       <Footer />
     </div>
-  )
-}
-
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-        <Routes>
-          <Route path ="/" element = {<Layout/>}>
-            <Route index element={<MainPage/>}/>
-            <Route path=":movieId" element={<DetailPage/>}/>
-            <Route path="search" element={<SearchPage/>}/>
-          </Route>
-        </Routes>
+    <div className="app">
+      <Routes> 
+        <Route path="/" element={<Layout />} >
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
